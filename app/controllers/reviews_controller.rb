91f54741a6c3bf7f64adac542a0f4caff2817943
lorraine-phill <lp3767-web@yahoo.com>
2013-10-14 12:24:20 +0100

    complete ratings given

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,8 +2,12 @@ class ReviewsController < ApplicationController
 
 	def create
 		@review = Review.new(params[:review].permit(:restaurant_id, :comment, :rating))
-        @review.save
-		redirect_to @review.restaurant
+        if @review.save
+			redirect_to @review.restaurant
+		else
+			flash[:error] = @review.errors.full_messages
+			redirect_to "/restaurants/#{@review.restaurant.id}"
+		end
 	
 		# if @restaurant.save
 		# 	redirect_to@restaurant
--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,10 @@ class Review < ActiveRecord::Base
 	  validates :comment, presence: true
 	  validates :rating, presence: true
 
+	  RATINGS = {1 => "Inedible", 2 => "Poor", 3 => "Average", 4 => "Good", 5 => "Delicious"}
+
+	  def text_rating
+	  	"#{rating}- #{RATINGS[rating]}"
+	  end
 
 end
